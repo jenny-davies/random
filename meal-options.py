dd79e34266ac776f0c716e3b1fe43fb7fcc01043
jenny-davies <jennyedavies@outlook.com>
2025-02-19 17:03:55 +0000

    added search function, not working yet

--- meal-options.py
+++ meal-options.py
@@ -1,9 +1,24 @@
-import mealsdict
+from mealsdict import meals
 
 def get_ingredients():
     ingredient = input('Enter a list of ingredients separated by commas: ')
     ingredient_list = [item.strip() for item in ingredient.split(',')]
     print(f'You want to use: {ingredient_list}')
 
-ingredients = get_ingredients()
+search_list = get_ingredients()
 
+def search_meals(ingredients):
+    options = []
+
+    for meal, details in meals.items():
+        ingredients = details.get('ingredients', [])
+        for ingredient in search_list:
+            if ingredient in ingredients:
+                options.append(meal)
+                break
+
+    return options
+
+all_meal_options = search_meals(search_list)
+
+print(f'You could make: {all_meal_options}')
